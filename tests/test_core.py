eba6a072b5cfc80c6f947ca1297e0119786b0948
Reuben Frankel <rfrankel@matatika.com>
2025-01-20 16:32:08 +0000

    Ignore some streams that currently return no records for our Veeqo test instance

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_veeqo.tap import TapVeeqo
 
@@ -11,4 +11,12 @@ SAMPLE_CONFIG = {}
 TestTapTrello = get_tap_test_class(
     tap_class=TapVeeqo,
     config=SAMPLE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records_for_streams=[
+            "product_option_specifics",
+            "product_properties",
+            "product_property_specifics",
+            "variant_property_specifics",
+        ]
+    ),
 )
